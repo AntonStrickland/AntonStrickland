7a1e25df3e34768631cea54ee5063779ba54dd4a
Kinjo <antonmstrickland@gmail.com>
2018-12-28 04:45:18 -0600

    Added donation address (test)

--- ethereum/donate.js
+++ ethereum/donate.js
@@ -4,7 +4,7 @@ import Donate from './build/Donate.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(Donate.interface),
-  process.env.DONATE_ADDR
+  '0xC519cFc62B6E2cACB15A49D195f697d7145F1EB3'
 );
 
 export default instance;
