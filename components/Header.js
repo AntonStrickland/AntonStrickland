1ffa1ccc5332f9c942a7172a904573f355e7d42a
Kinjo <antonmstrickland@gmail.com>
2019-01-01 23:48:56 -0600

    Started work on the other pages

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,9 @@ export default () => {
       <Link route="/"><a className="item">Anton Strickland</a></Link>
 
       <Menu.Menu position="right">
-        <Link route="/"><a className="item">Resume</a></Link>
-        <Link route="/"><a className="item">Projects</a></Link>
-        <Link route="/"><a className="item">Services</a></Link>
+        <Link route="https://drive.google.com/file/d/1gKNzpDTSxXLuVB4H1jdjpQlVzceXKP6K/view?usp=sharing"><a className="item">Resume</a></Link>
+        <Link route="/projects"><a className="item">Projects</a></Link>
+        <Link route="/services"><a className="item">Services</a></Link>
         <Link route="/contact"><a className="item">Contact</a></Link>
         <Link route="/donate"><a className="item">Donate</a></Link>
       </Menu.Menu>
---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+import DonateForm from '../components/DonateForm.js';
+import Layout from '../components/Layout.js';
+
+export default () => (
+  <Layout>
+    <div>
+      <h2>Projects</h2>
+      <p>Under construction.</p>
+    </div>
+  </Layout>
+)
