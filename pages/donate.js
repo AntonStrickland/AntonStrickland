b0a11216d151158a465ce341eb363abb0583a469
Kinjo <antonmstrickland@gmail.com>
2019-01-03 23:28:43 -0600

    Added link to Coinbase

--- pages/donate.js
+++ pages/donate.js
@@ -14,7 +14,7 @@ export default () => (
       <h3>Install MetaMask</h3>
       <p><a href="https://metamask.io/">MetaMask</a> is an extension for the Firefox, Chrome, and Brave browsers that allows anyone to easily interact with Ethereum-based smart contracts through the web. You will need to install it on your browser and set up your wallet before you can send me any donations.</p>
       <h3>Buy Ethereum</h3>
-      <p>Ethereum can be purchased through many exchanges, the most popular being Coinbase. Open MetaMask and click 'buy' to select an exchange and purchase some ether. If you attempt a transaction without having enough ether, MetaMask should automatically ask if you want to buy more.</p>
+      <p>Ethereum can be purchased through an exchange such as <a href="https://www.coinbase.com/join/59a998af24ce74029ab84abd">Coinbase.</a> You can transfer ether to your MetaMask wallet from Coinbase. If you attempt a transaction without having enough ether, MetaMask should automatically ask if you want to buy more.</p>
       <h3>Submit the Form</h3>
       <p>Just specify how much you'd like to donate (in ether) and press the button. MetaMask should open a window asking you to confirm the transaction. Please note that it may take up to a minute for the transfer to finish processing. Thank you!</p>
       <hr/>
